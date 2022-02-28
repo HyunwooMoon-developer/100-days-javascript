288e11fc9dc864542ac06fa1002b0ea78b6757c7
Hyunwoo Moon <mhw9163@gmail.com>
2022-02-27 20:07:43 -0800

    day 51 & 52

---
+++ day-51-count-up-app/script.js
@@ -0,0 +1,25 @@
+const counters = document.querySelectorAll('.counter');
+
+counters.forEach((counter) => {
+  counter.innerText = 0;
+
+  function updateCounter() {
+    const target = +counter.dataset.target;
+    const count = +counter.innerText;
+    const increment = target / 200;
+    if (count < target) {
+      counter.innerText = `${Math.ceil(count + increment)}`;
+      setTimeout(updateCounter, 10);
+    } else {
+      counter.innerText = target;
+    }
+  }
+  window.addEventListener('scroll', () => {
+    const scrollHeight = window.pageYOffset;
+    const sectionTop = document.querySelector('.top');
+    const sectionTopHeight = sectionTop.clientHeight;
+    if (scrollHeight >= sectionTopHeight - 1) {
+      updateCounter();
+    }
+  });
+});
